32a1fb7fd8905857802425ede1ea3301138fb5c3
HaoNguyen <>
2025-10-21 23:05:27 +0700

    Update colors and add HButtonStyle

--- SwiftUI-BaseApp/SwiftUI-BaseApp/Components/Extensions/View+Additional.swift
+++ SwiftUI-BaseApp/SwiftUI-BaseApp/Components/Extensions/View+Additional.swift
@@ -66,3 +66,41 @@ struct BackgroundImageModifier: ViewModifier {
         
     }
 }
+
+// MARK: - RoundedCorner
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(
+                width: radius,
+                height: radius
+            )
+        )
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func roundCorners(
+        _ radius: CGFloat,
+        corners: UIRectCorner = .allCorners
+    ) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+    
+    func roundedBorder(
+        cornerRadius: CGFloat,
+        lineWidth: CGFloat = 1,
+        borderColor: Color = .clear
+    ) -> some View {
+        overlay( /// apply a rounded border
+            RoundedRectangle(cornerRadius: cornerRadius)
+                .stroke(borderColor, lineWidth: lineWidth)
+        )
+    }
+}
