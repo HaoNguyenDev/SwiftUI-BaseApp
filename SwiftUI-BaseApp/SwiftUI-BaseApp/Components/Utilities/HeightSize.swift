86781bae567369fccae072d13736c805050777c7
HaoNguyen <>
2025-10-26 15:39:57 +0700

    Update HButton tertiary style with icon

--- SwiftUI-BaseApp/SwiftUI-BaseApp/Components/Utilities/HeightSize.swift
+++ SwiftUI-BaseApp/SwiftUI-BaseApp/Components/Utilities/HeightSize.swift
@@ -12,4 +12,6 @@ struct HeightSize {
     static let dfTextField: CGFloat = 55.0
     static let dfButton: CGFloat = 50.0
     static let headerIcon: CGFloat = 30.0
+    static let line05px: CGFloat = 0.5
+    static let line1px: CGFloat = 1.0
 }
